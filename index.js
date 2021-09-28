fc504d17ff45c41b8c75b29bd7115456efc1514d
snygghugo <91157834+snygghugo@users.noreply.github.com>
2021-09-29 00:07:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // Require the necessary discord.js classes "klient" är alltså botten pretty much
 const { Client, Intents, Message, Channel, TextChannel } = require('discord.js');
 const { token } = require('./config.json');
-var fs = require('fs');
+var filGöraren = require('fs');
 
 const pinns = '873614838692192286'
 // Create a new client instance
@@ -19,7 +19,6 @@ client.once('ready', () => {
 
 	const kanalen = client.channels.cache.get(pinns);
 	kanalen.messages.fetch({ limit: 100 }).then(messages => {
-		console.log('baby!');
 		let telegram = messages.map(meddelande => ({ //Deklarar en funktion som input till messages.map //Två paranteser säger att den här kommer direkt bli en grej jag vill
 			litteratör: meddelande?.embeds[0]?.author?.name, //kommatecken avgränsar mellan två fält i det jag returnar
 			dikt: meddelande?.embeds[0]?.description, //frågetecken gör att min funktion inte skiter på sig, jag använder dem eftersom jag inte får göra if satser
@@ -28,13 +27,12 @@ client.once('ready', () => {
 			hyperövrigbonuslänkar: meddelande?.embeds[0]?.fields?.slice(1)?.map(
 				hyperfält => hyperfält?.value?.match(/\(([:/\w.]+)/i)[1]),
 		}))
-
-		var json = JSON.stringify(telegram) + '\r\n';
-		//fs.appendFile('Bigpinns.json', json, 'utf8', function (err) {
-		//	if (err) throw err;
+		let JAAAAYSOOOOOon = JSON.stringify(telegram) + '\r\n';
+		//filGöraren.appendFile('Bigpinns.json', JAAAAYSOOOOOon, 'utf8', function (whoops) {
+		//	if (whoops) throw whoops;
 		//	console.log('complete');
-		//});
-
+		//}); DEN HÄR SKA AKTIVERAS SEN NÄR BOTTEN ÄR REDO FÖR RIKTIG BUSINESS
+		
 
 
 		console.log(telegram)
@@ -48,7 +46,7 @@ client.once('ready', () => {
 });
 
 const prefix = "hej ";
-const haranglängd = 63
+const haranglängd = 200
 
 var svampbob = function (harang) {
 	var chars = harang.toLowerCase().split("");
@@ -63,43 +61,41 @@ var svampbob = function (harang) {
 
 
 client.on("messageCreate", (meddelande) => {  //=> är en funktion
-	//if (meddelande.channelId == pinns && meddelande.author.id == "873614862578769940" && meddelande.embeds[0]) {
+	//if (meddelande.channelId == pinns && meddelande.author.id == "873614862578769940" && meddelande.embeds[0]) { den här är sparad eftersom den har NQN botten
 	console.log(meddelande.content.length)
-	if (meddelande.content.length >= haranglängd && meddelande.author.id !== "745345949295181886") {
+	var aleaIactaEst = Math.floor(Math.random() * 20)
+	console.log('Tärningen är kastad! ' + aleaIactaEst)
+	if (aleaIactaEst == 19 || meddelande.content.length >= haranglängd && meddelande.author.id !== "745345949295181886") {
 
 		let jamesCameron = 'https://cdn.discordapp.com/avatars/' + meddelande.author.id + '/' + meddelande.author.avatar
-		// KOLLA IGENOM DENNA https://discordjs.guide/popular-topics/webhooks.html#sending-messages verkar som att jag kanske måste göra flera webhooks? 
 		async function webbKrok() {
 			try {
 				const channel = client.channels.cache.get(meddelande.channel.id);
-				const webhooks = await channel.fetchWebhooks();
-				const webhook = webhooks.first();
-	
+				var webhook = await channel.createWebhook('"' + svampbob(meddelande.member.displayName) + '"', {
+					avatar: jamesCameron,
+				})
+				let b = 2
+
 				await webhook.send({
 					content: '**' + svampbob(meddelande.content) + '**',
-					username: '"'+ meddelande.member.displayName + '"',
+					username: '"' + svampbob(meddelande.member.displayName) + '"',
 					avatarURL: jamesCameron,
 				});
-			} catch (error) {
-				console.error('Error trying to send a message: ', error);
+				webhook.delete()
+			} catch (whoops) {
+				console.error('Error trying to send a message: ', whoops);
 			}
 		}
 		webbKrok()
-		let a = 2
 
-		//meddelande.channel.send('**' + svampbob(meddelande.content) + '**') DEN HÄR ÄR VIKTIG ATT TA TILLBAKA
-		//
-		//meddelande.channel.createWebhook('Some-username', {
-		//	avatar: 'https://i.imgur.com/AfFp7pu.png',
-		//})
 	}
 	else {
-
-		if (meddelande.content.endsWith('?') && meddelande.author.id !== "745345949295181886") meddelande.reply('Bra fråga, återkommer :)');
-		if (!meddelande.content.startsWith(prefix)) return; //det här fattar tom jag :) 
-
-		const commandBody = meddelande.content.slice(prefix.length); // tar meddelandet som vi fått med prefixet, tar bort så många bokstäver
-		//som prefixet är
+		let dravel = meddelande.content.toLowerCase()
+		if (dravel.endsWith('?') && meddelande.author.id !== "745345949295181886") meddelande.reply('Bra fråga, återkommer :)');
+		if (dravel === 'hey guys') { meddelande.reply('https://www.youtube.com/watch?v=fqoM2BJ6_-8') }
+		if (!dravel.startsWith(prefix)) return; //det här fattar tom jag :) 
+		let b = 2
+		const commandBody = dravel.slice(prefix.length); // tar meddelandet som vi fått med prefixet, tar bort så många bokstäver som prefixet är
 		const args = commandBody.split(' '); //skapar "en array of sub-strings" för allt som är mellanslag. Denna heter "args
 		const command = args.shift().toLowerCase(); //gör allt som finns i args till lowercase, och kallar allt för command   
 
